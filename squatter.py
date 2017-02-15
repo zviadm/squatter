e801824907536c432b3313175540a08119ae40a0
Zviad Metreveli <zviad@dropbox.com>
2017-02-15 15:10:50 -0500

    better selector file loading

--- squatter.py
+++ squatter.py
@@ -1,24 +1,33 @@
 import json
 import os
 
+
 import cv2
 from pymediainfo import MediaInfo
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.factory import Factory
 from kivy.graphics.texture import Texture
-from kivy.graphics import Color, Rectangle
-from kivy.uix.widget import Widget
+from kivy.graphics import Color, Rectangle, Line, InstructionGroup
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.slider import Slider
 from kivy.uix.relativelayout import RelativeLayout
 
+from track_squat import extract_squat_reps, _cm, _sq_distance
 
-_FILENAME = "squat1.mov"
+_SQUATTER_EXT=".squatter"
 
-class SquatterWidget(Widget):
-    pass
+class LoadDialog(FloatLayout):
+    cwd = os.getcwd()
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 class FrameCapture(object):
 
@@ -127,7 +136,6 @@ class FrameCapture(object):
         ok = self._tracker.init(frame, self._track_windows[-1])
         assert ok, "Failed to initialize tracker!"
 
-
     def track_next(self):
         """Should be called until returns False"""
         print ("FRAME N", self._cap.get(cv2.CAP_PROP_POS_FRAMES), self._track_windows[-1])
@@ -152,19 +160,118 @@ class FrameCanvas(RelativeLayout):
         #self.bind(mouse_pos=self._on_hover)
         self.touch_points = []
 
-    def on_touch_down(self, touch):
-        if not self._app._cap: return False
-        canvas_xy = (touch.pos[0]-self.pos[0], touch.pos[1]-self.pos[1])
-        print ("Canvas XY", canvas_xy)
-        frame_xy = self._app._cap.canvas_xy_to_frame_xy(*canvas_xy)
-        frame_n = self._app._frame_slider.value
-        print ("Point on Frame", frame_xy, frame_n)
-        self.touch_points = self.touch_points[-1:] + [frame_xy]
-        return False
+        self._select_event = None
+        self._select_center_xy = None
+        self._select_radius = None
+        self._select_circle = None
 
-    def on_hover(self, pos):
-        if not self._app._cap: return False
-        pass
+    def on_touch_down(self, touch):
+        if not self.collide_point(*touch.pos): return False
+        if self._app._cap is None: return False
+
+        canvas_xy = (touch.pos[0]-self.pos[0], touch.pos[1]-self.pos[1])
+        if ((self._select_center_xy is None) or
+                (_sq_distance(canvas_xy, self._select_center_xy) >= self._select_radius ** 2)):
+            self._select_center_xy = canvas_xy
+            self._select_radius = 0
+            if self._select_circle is not None:
+                self.canvas.remove(self._select_circle)
+                self._select_circle = None
+
+        if self._select_event is not None:
+            self._select_event.cancel()
+        self._select_event = Clock.schedule_interval(self._inc_selection, 0.01)
+        return True
+
+    def _inc_selection(self, *args, **kwargs):
+        self._select_radius += 1
+        frame_xy1 = self._app._cap.canvas_xy_to_frame_xy(
+                self._select_center_xy[0] - self._select_radius,
+                self._select_center_xy[1] - self._select_radius)
+        frame_xy2 = self._app._cap.canvas_xy_to_frame_xy(
+                self._select_center_xy[0] + self._select_radius,
+                self._select_center_xy[1] + self._select_radius)
+        if frame_xy1 is None or frame_xy2 is None:
+            self._select_radius -= 1
+            return
+        if self._select_circle is not None:
+            self.canvas.remove(self._select_circle)
+        self._select_circle = InstructionGroup()
+        self._select_circle.add(Color(1, 0, 0))
+        self._select_circle.add(
+                Line(circle=(self._select_center_xy) + (self._select_radius, ), width=5))
+        self.canvas.add(self._select_circle)
+
+    def on_touch_up(self, touch):
+        if self._select_event is not None:
+            self._select_event.cancel()
+            self._select_event = None
+
+    def clear_selection(self):
+        self.on_touch_up(None)
+        if self._select_circle is not None:
+            self.canvas.remove(self._select_circle)
+            self._select_circle = None
+        self._select_center_xy = None
+        self._select_radius = None
+
+    def get_selection(self):
+        if self._select_center_xy is None:
+            return None
+        frame_xy1 = self._app._cap.canvas_xy_to_frame_xy(
+                self._select_center_xy[0] - self._select_radius,
+                self._select_center_xy[1] - self._select_radius)
+        frame_xy2 = self._app._cap.canvas_xy_to_frame_xy(
+                self._select_center_xy[0] + self._select_radius,
+                self._select_center_xy[1] + self._select_radius)
+        return (frame_xy1, frame_xy2)
+
+class SquatRepCanvas(RelativeLayout):
+
+    def __init__(self, app, track_windows, bottom_idx, start_frame, **kwargs):
+        super(SquatRepCanvas, self).__init__(**kwargs)
+        self._app = app
+        self._start_frame = start_frame
+
+        self._cms = [_cm(w) for w in track_windows]
+        self._min_x = min(cm[0] for cm in self._cms)
+        self._min_y = min(cm[1] for cm in self._cms)
+        self._max_x = max(cm[0] for cm in self._cms)
+        self._max_y = max(cm[1] for cm in self._cms)
+        self._bottom_idx = bottom_idx
+        self._start_frame = start_frame
+        self.bind(width=self._redraw)
+        self.bind(height=self._redraw)
+
+    def _redraw(self, *args, **kwargs):
+        scale = (self.height * 0.9) / (self._max_y - self._min_y)
+        x_adj = (self.width - ((self._max_x - self._min_x) * scale)) / 2
+        y_adj = (self.height - ((self._max_y - self._min_y) * scale)) / 2
+
+        def _convert_xy(xy):
+            return (
+                    (xy[0] - self._min_x) * scale + x_adj,
+                    self.height - ((xy[1] - self._min_y) * scale + y_adj))
+
+        _line_width = 5
+        with self.canvas:
+            Color(0, 0, 1)
+            Line(points=(
+                _convert_xy(self._cms[0]) +
+                _convert_xy(self._cms[self._bottom_idx])), width=_line_width)
+            Color(1, 0, 0)
+            Line(points=[p
+                for cm in self._cms[:self._bottom_idx]
+                for p in _convert_xy(cm)], width=_line_width)
+            Color(0, 1, 0)
+            Line(points=[p
+                for cm in self._cms[self._bottom_idx:]
+                for p in _convert_xy(cm) ], width=_line_width)
+
+    def on_touch_down(self, touch):
+        if not self.collide_point(*touch.pos): return False
+        self._app.change_frame_to(self._start_frame)
+        return True
 
 
 class SquatterApp(App):
@@ -178,18 +285,28 @@ class SquatterApp(App):
         process_btn = Button(text='Process')
         process_btn.bind(on_press=self._process_video)
 
-        main_layout = GridLayout(cols=1)
-        main_layout.add_widget(frame_canvas)
-        main_layout.add_widget(frame_slider)
         btn_layout = GridLayout(cols=2, size_hint_y=None, height=100)
         btn_layout.add_widget(load_btn)
         btn_layout.add_widget(process_btn)
-        main_layout.add_widget(btn_layout)
+        video_layout = GridLayout(cols=1)
+        video_layout.add_widget(frame_canvas)
+        video_layout.add_widget(frame_slider)
+        video_layout.add_widget(btn_layout)
+
+        rep_layout_inner = GridLayout(cols=1, size_hint_y=None)
+        rep_layout_inner.bind(minimum_height=rep_layout_inner.setter('height'))
+        rep_layout = ScrollView(size_hint_x=None, width=400)
+        rep_layout.add_widget(rep_layout_inner)
+
+        main_layout = GridLayout(cols=2)
+        main_layout.add_widget(video_layout)
+        main_layout.add_widget(rep_layout)
 
         self._frame_canvas = frame_canvas
         self._frame_slider = frame_slider
         self._btn_layout = btn_layout
         self._cap = None
+        self._rep_layout = rep_layout_inner
 
         _keyboard = None
         def _keyboard_closed():
@@ -218,61 +335,86 @@ class SquatterApp(App):
         # TODO(zviad): Figure out how to update UI when Key is held.
         return True
 
+    def _dismiss_popup(self):
+        self._popup.dismiss()
 
     def _load_video(self, instance):
+        content = LoadDialog(load=self._load_video_file, cancel=self._dismiss_popup)
+        self._popup = Popup(title="Select video", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def _load_video_file(self, path, filename):
         if self._cap:
             self._cap.release()
         track_first_frame = None
         track_windows = None
-        if os.path.exists(_FILENAME+".squatter"):
-            with open(_FILENAME+".squatter", "r") as f:
+        filepath = os.path.join(path, filename[0])
+        self._squatter_file = filepath+_SQUATTER_EXT
+        if os.path.exists(self._squatter_file):
+            with open(self._squatter_file, "r") as f:
                 tracking_data = json.loads(f.read())
                 track_first_frame = tracking_data["first_frame"]
                 track_windows = tracking_data["track_windows"]
         self._cap = FrameCapture(
-                _FILENAME, self._frame_canvas,
-                track_first_frame=track_first_frame, track_windows=track_windows)
+            filepath, self._frame_canvas,
+            track_first_frame=track_first_frame, track_windows=track_windows)
+        self._process_tracking_info()
 
         self._frame_slider.max = self._cap.n_frames()-1
-        self._frame_slider.value = 0
-        self.seek_video(self._frame_slider, self._frame_slider.value)
+        self.change_frame_to(track_first_frame or 0)
+        self.seek_video(None, None)
+        self._dismiss_popup()
+
+    def _process_tracking_info(self):
+        self._rep_layout.clear_widgets()
+
+        track_first_frame, track_windows = self._cap._track_first_frame, self._cap._track_windows
+        if track_first_frame is None: return
+        squat_reps = extract_squat_reps(track_windows)
+        for rep_idx, squat_rep in enumerate(squat_reps):
+            self._rep_layout.add_widget(
+                    Label(text="Rep {}".format(rep_idx+1), size_hint_y=None, height=50))
+            self._rep_layout.add_widget(
+                    SquatRepCanvas(
+                        self, track_windows[squat_rep[0]:squat_rep[2]],
+                        squat_rep[1]-squat_rep[0], squat_rep[0] + track_first_frame,
+                        size_hint_y=None, height=400))
 
     def _process_video(self, instance):
-        if not self._cap: return
+        points = self._frame_canvas.get_selection()
+        if points is None: return
 
-        # For testvid3:
-        # self._cap.track_start(259, 410, 409-259, 560-410, 0)
-        # For squatvid: self._cap.track_start(505, 4, 544-505, 38-4, 596)
-        # For testvid2: self._cap.track_start(278, 245, 474-278, 445-245, 0)
-        points = self._frame_canvas.touch_points
-        if len(points) < 2: return
+        self._frame_slider.disabled = True
+        self._btn_layout.disabled = True
         self._cap.track_start(
                 points[0][0], points[0][1],
                 points[1][0]-points[0][0], points[1][1]-points[0][1],
                 int(self._frame_slider.value))
-        self._frame_slider.disabled = True
-        self._btn_layout.disabled = True
-        self.seek_video(self._frame_slider, self._frame_slider.value)
         interval_secs = 0.5/self._cap.fps() # 2x original speed.
         def _track_it(dt):
             f = self._cap.track_next()
             if f is None:
                 self._frame_slider.disabled = False
                 self._btn_layout.disabled = False
+                self._process_tracking_info()
 
                 # TODO(zviad): Store tracking points as JSON.
                 d = {
                     "first_frame": self._cap._track_first_frame,
                     "track_windows": self._cap._track_windows}
-                with open(_FILENAME+".squatter", "w") as f:
+                with open(self._squatter_file, "w") as f:
                     f.write(json.dumps(d))
                 return
             self._frame_slider.value += 1
             Clock.schedule_once(_track_it, interval_secs)
         Clock.schedule_once(_track_it, interval_secs)
 
+    def change_frame_to(self, frame_n):
+        self._frame_slider.value = frame_n
+
     def seek_video(self, _i, _v):
         if not self._cap: return
+        self._frame_canvas.clear_selection()
         frame_pos, frame = self._cap.frame_for_canvas(int(self._frame_slider.value))
         # Frame needs to flipped because Kivy coordinates are bottom up.
         frame = cv2.flip(frame, 0)
@@ -291,6 +433,9 @@ class SquatterApp(App):
             self._frame_canvas.canvas.add(
                     Rectangle(pos=track_window[:2], size=track_window[2:]))
         self._frame_canvas.canvas.ask_update()
+
+Factory.register('LoadDialog', cls=LoadDialog)
+
 if __name__ == '__main__':
     SquatterApp().run()
 
---
+++ track_squat.py
@@ -0,0 +1,49 @@
+def _cm(track_window):
+    """Center of mass for tracking window."""
+    return (track_window[0] + track_window[2]/2, track_window[1] + track_window[3]/2)
+
+def _sq_distance(cm1, cm2):
+    return (cm1[0]-cm2[0])**2 + (cm1[1]-cm2[1])**2
+
+def extract_squat_reps(track_windows):
+    reps = []
+    min_squat_distance = 2*track_windows[0][3]
+    min_back_range = 1/2 * track_windows[0][3]
+
+    idx = 0
+    while idx < len(track_windows):
+        start_cm = _cm(track_windows[idx])
+        start_idx = idx
+        max_cm = _cm(track_windows[idx])
+        max_idx = idx
+        while True:
+            if idx >= len(track_windows):
+                # No REP. But ran out of tracking frames.
+                break
+            cur_cm = _cm(track_windows[idx])
+            if cur_cm[1] > max_cm[1]:
+                max_cm = cur_cm
+                max_idx = idx
+
+            if ((max_cm[1] > start_cm[1] + min_squat_distance) and
+                (cur_cm[1] < start_cm[1] + min_back_range)):
+                # We have found bottom of the Squat, which is at w_max_cm.
+                # Now time to find finishing frame.
+                end_dst = _sq_distance(cur_cm, start_cm)
+                end_idx = idx
+                idx += 1
+                while idx < len(track_windows):
+                    cur_cm = _cm(track_windows[idx])
+                    if cur_cm[1] > start_cm[1] + 2*min_back_range:
+                        break
+                    cur_dst = _sq_distance(cur_cm, start_cm)
+                    if cur_dst < end_dst:
+                        end_dst = cur_dst
+                        end_idx = idx
+                    idx += 1
+                reps.append((start_idx, max_idx, end_idx))
+                idx = end_idx
+                break
+            idx += 1
+    return reps
+
